5d7b4c79b9019301f32a8119de606bea6b3887f9
Alice Moretti <ali.moret88@gmail.com>
2022-08-29 11:25:14 +0300

    Added state to GoalInput to erase input text when clicking on submit button

--- src/components/goals/GoalInput.js
+++ src/components/goals/GoalInput.js
@@ -1,19 +1,23 @@
+import { useState } from "react";
+
 function GoalInput(props) {
-  let goal;
+  const [goal, setGoal] = useState("");
+
   const goalHandler = (e) => {
-    goal = e.target.value;
+    setGoal(e.target.value);
   };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
     props.onAddGoal(goal);
+    setGoal("");
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label>Course Goal</label>
-        <input type="text" onChange={goalHandler} />
+        <input value={goal} type="text" onChange={goalHandler} />
         <button>submit</button>
       </div>
     </form>
