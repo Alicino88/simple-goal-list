acddeb71766cb11e82e8f6024205a6c446b95eef
Alice Moretti <ali.moret88@gmail.com>
2022-08-29 09:18:20 +0300

    App receives data from GoalInput component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import GoalInput from "./components/goals/GoalInput";
+import { useState } from "react";
 function App() {
+  const addGoalHandler = (goal) => {
+    console.log(goal);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GoalInput onAddGoal={addGoalHandler} />
     </div>
   );
 }
