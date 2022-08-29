62b5dd822ec16981f889aa42d87fa582db228ec4
Alice Moretti <ali.moret88@gmail.com>
2022-08-29 09:46:45 +0300

    Stored new goals inside state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import GoalInput from "./components/goals/GoalInput";
 import { useState } from "react";
+
 function App() {
+  const [myGoals, setMyGoals] = useState([
+    { text: "baking an amazing cake", id: "g1" },
+    { text: "learn Redux", id: "g2" },
+  ]);
+
+  /*goal is the input text coming from the GoalInput component */
+  /*previousGoals is the initial state made up of the above array with two objects */
+
   const addGoalHandler = (goal) => {
     console.log(goal);
+    setMyGoals((previousGoals) => {
+      const updatedGoals = [...previousGoals];
+      /*Below we add,to the beginning of the updatedGoals array, the coming goal from the form and attach an unique id to it */
+      updatedGoals.unshift({ text: goal, id: Math.random().toString() });
+      console.log(updatedGoals);
+      return updatedGoals;
+    });
   };
+
   return (
-    <div className="App">
-      <GoalInput onAddGoal={addGoalHandler} />
+    <div>
+      <div className="App">
+        <GoalInput onAddGoal={addGoalHandler} />
+      </div>
+      <div></div>
     </div>
   );
 }
