80a49e5281ab4799fb6044cb02462b12845b1f9a
Alice Moretti <ali.moret88@gmail.com>
2022-08-29 11:22:46 +0300

    GoalList renders single goals and receives and passes goal id to App component

--- src/components/goals/GoalList.js
+++ src/components/goals/GoalList.js
@@ -1,10 +1,21 @@
+import GoalItem from "./GoalItem";
+
 function GoalList(props) {
+  const removeGoal3 = (id) => {
+    props.removeGoal4(id);
+    /*console.log(id);*/
+  };
   return (
-    <div>
+    <ul>
       {props.entries.map((entry) => (
-        <p>{entry.text}</p>
+        <GoalItem
+          key={entry.id}
+          id={entry.id}
+          title={entry.text}
+          removeGoal2={removeGoal3}
+        />
       ))}
-    </div>
+    </ul>
   );
 }
 
