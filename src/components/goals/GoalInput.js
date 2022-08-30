91da31a0319b4e5b7bda0d63c731273f61fc2aec
Alice Moretti <ali.moret88@gmail.com>
2022-08-30 10:18:28 +0300

    Added dynamic inline style to form

--- src/components/goals/GoalInput.js
+++ src/components/goals/GoalInput.js
@@ -2,13 +2,23 @@ import { useState } from "react";
 
 function GoalInput(props) {
   const [goal, setGoal] = useState("");
+  /*Below we add state to re-render UI when user clicks on submit but the input field is empty */
+  const [isValid, setIsValid] = useState(true);
 
   const goalHandler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
+    }
     setGoal(e.target.value);
   };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
+
+    if (goal.trim().length === 0) {
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(goal);
     setGoal("");
   };
@@ -16,8 +26,18 @@ function GoalInput(props) {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input value={goal} type="text" onChange={goalHandler} />
+        <label style={{ color: !isValid ? "#FF6542" : "black" }}>
+          Course Goal
+        </label>
+        <input
+          style={{
+            borderColor: !isValid ? "#564154" : "#ccc",
+            background: !isValid ? "#FF6542" : "transparent",
+          }}
+          value={goal}
+          type="text"
+          onChange={goalHandler}
+        />
         <button>submit</button>
       </div>
     </form>
